4d271edc296bc3c82b14b8127bf2fbd1c8ec970b
witch <skpepe134@gmail.com>
2018-12-24 15:24:14 +0700

    add some filter choice to filter dialog

--- src/app/components/filter-dialog/filter-dialog.component.ts
+++ src/app/components/filter-dialog/filter-dialog.component.ts
@@ -1,22 +1,29 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-filter-dialog',
   templateUrl: './filter-dialog.component.html',
-  styleUrls: ['./filter-dialog.component.scss']
+  styleUrls: ['./filter-dialog.component.scss'],
 })
 export class FilterDialogComponent implements OnInit {
-
+  landscape = false;
+  portrait = false;
+  isMobile = false;
   constructor(
     public dialogRef: MatDialogRef<FilterDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data,
-  ) {}
+    breakpointObserver: BreakpointObserver,
+  ) {
+    breakpointObserver.observe([Breakpoints.Handset]).subscribe(result => {
+      this.isMobile = result.matches;
+    });
+  }
 
   onNoClick(): void {
     this.dialogRef.close();
   }
   ngOnInit() {}
-  submit() {
-  }
+  submit() {}
 }
