3b427326fba9857e3d0954e55e73caeba66cdd1f
witch <skpepe134@gmail.com>
2018-12-24 16:09:14 +0700

    finish filter dialog

--- src/app/components/filter-dialog/filter-dialog.component.ts
+++ src/app/components/filter-dialog/filter-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { FilterService } from 'src/app/services/filter.service';
 
 @Component({
   selector: 'app-filter-dialog',
@@ -11,10 +12,29 @@ export class FilterDialogComponent implements OnInit {
   landscape = false;
   portrait = false;
   isMobile = false;
+  camera = '';
+  mood = {
+    happy: false,
+    sad: false,
+    cloudy: false,
+    rainy: false,
+    snowy: false,
+    lonely: false,
+  };
+  tone = {
+    warm: false,
+    cool: false,
+    bw: false,
+    contrast: false,
+    saturate: false,
+    film: false,
+    street: false,
+  };
   constructor(
     public dialogRef: MatDialogRef<FilterDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data,
     breakpointObserver: BreakpointObserver,
+    private filter: FilterService,
   ) {
     breakpointObserver.observe([Breakpoints.Handset]).subscribe(result => {
       this.isMobile = result.matches;
@@ -26,4 +46,30 @@ export class FilterDialogComponent implements OnInit {
   }
   ngOnInit() {}
   submit() {}
+  selectAll(event) {
+    const checked = event.checked;
+    this.landscape = event.checked;
+    this.portrait = event.checked;
+    for (const key in this.mood) {
+      if (this.mood.hasOwnProperty(key)) {
+        this.mood[key] = checked;
+      }
+    }
+    for (const key in this.tone) {
+      if (this.tone.hasOwnProperty(key)) {
+        this.tone[key] = checked;
+      }
+    }
+  }
+  search() {
+    this.filter.setFilter(
+      {
+        portrait: this.portrait,
+        landscape: this.landscape,
+      },
+      this.mood,
+      this.tone,
+      this.camera,
+    );
+  }
 }
